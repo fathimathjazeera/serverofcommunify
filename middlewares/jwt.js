d0a17173e55f96b497b43de3bf63e8260d6fb75c
fathimathjazeera <jezijazeera@gmail.com>
2023-12-19 16:25:16 +0530

    updated

--- middlewares/jwt.js
+++ middlewares/jwt.js
@@ -1,13 +1,11 @@
 const jwt=require('jsonwebtoken')
 require('dotenv').config();
 module.exports=(req,res,next)=>{
-    
     try{
     let authHeader=req.headers.authorization
     if(authHeader==undefined){
         res.status(401).send({error:"no token provided"})
     }
-
     let token=authHeader.split(" ")[1]
     console.log(token,"token from middleware");
    const verified= jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,function(err,decoded){
